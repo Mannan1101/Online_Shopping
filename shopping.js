6dbc76819369699189de4c82b9933f3ae2eb2dc9
Abdul Mannan <161303446+Mannan1101@users.noreply.github.com>
2024-03-25 02:18:14 +0500

    Add files via upload

---
+++ shopping.js
@@ -0,0 +1,18 @@
+// Ramdan Rashan packege for needy people
+// JDC Online shopping Store
+var totalAmount = 0;
+var Rice = 305;
+var roohafza = 390;
+var flour = 895;
+var tapaltea = 675;
+var daalchanna = 264;
+var daalmong = 245;
+var besan = 249;
+var cookingoil = 480;
+var salt = 60;
+var dates = 670;
+var vermicelli = 500;
+var Redchillipowder = 670;
+var Shansmasala = 565;
+totalAmount = Rice + roohafza + flour + tapaltea + daalchanna + daalmong + besan + cookingoil + salt + dates + vermicelli + Redchillipowder + Shansmasala;
+console.log(totalAmount);
---
+++ shopping.ts
@@ -0,0 +1,23 @@
+
+// Ramdan Rashan packege for needy people
+// JDC Online shopping Store
+
+let totalAmount:number = 0;
+
+let Rice:number = 305;
+let roohafza:number = 390;
+let flour:number = 895;
+let tapaltea:number = 675;
+let daalchanna:number = 264;
+let daalmong:number = 245
+let besan:number = 249;
+let cookingoil:number = 480;
+let salt:number = 60;
+let dates:number = 670;
+let vermicelli:number = 500;
+let Redchillipowder:number = 670;
+let Shansmasala:number = 565
+
+totalAmount = Rice+roohafza+flour+tapaltea+daalchanna+daalmong+besan+cookingoil+salt+dates+vermicelli+Redchillipowder+Shansmasala
+
+console.log(totalAmount);
